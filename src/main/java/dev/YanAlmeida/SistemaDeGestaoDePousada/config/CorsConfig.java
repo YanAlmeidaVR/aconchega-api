4c4ca1929eb26bc6cdab26f0b412fa84a7f8e5d8
Yan Almeida <yandealmeida07@outlook.com>
2026-01-29 22:38:57 -0300

    fix: Problema de conexão do banco de dados resolvido

--- src/main/java/dev/YanAlmeida/SistemaDeGestaoDePousada/config/CorsConfig.java
+++ src/main/java/dev/YanAlmeida/SistemaDeGestaoDePousada/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${spring.web.cors.allowed-origins}")
+    @Value("${spring.web.cors.allowed-origins:http://localhost:3000}")
     private String allowedOrigins;
 
     @Override
